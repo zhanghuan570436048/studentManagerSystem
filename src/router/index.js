687d86a5124f0192ba8c2240751646fd508eafe1
zhanghuan <zhang_huan@cestc.cn>
2022-06-15 08:53:15 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Index from '@/views/index.vue'
+
 
 Vue.use(Router)
 
@@ -8,18 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
     },
-    {
-      path: '/map',
-      name: 'map01',
-      component: ()=>import('../components/map/demo01.vue')
-    },
-    {
-      path: '/map2',
-      name: 'map02',
-      component: ()=>import('../components/map/demo02.vue')
-    }
+    // {
+    //   path: '/map',
+    //   name: 'map01',
+    //   component: ()=>import('../components/map/demo01.vue')
+    // },
+    // {
+    //   path: '/map2',
+    //   name: 'map02',
+    //   component: ()=>import('../components/map/demo02.vue')
+    // }
   ]
 })
---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+<script>
+export default {
+    name:"souye",
+    data(){
+        return {
+            msg:'souye'
+        }
+    }
+}
+</script>
